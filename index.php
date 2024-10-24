58498e5251e22a786289f40d8711a8222f742aaa
KilianDrz <kilian.drzewiecki@zsz.bobowa.pl>
2024-10-24 08:30:11 +0200

    Pobieranie i Wyświetlanie Danych

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "szkola";
+$dbname = "szkola1";
 
 // Tworzenie połączenia
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -19,6 +19,19 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Błąd połączenia: " . mysqli_connect_error());
 }
+$sql = "SELECT * FROM uczniowie";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Brak wyników";
+}
+mysqli_close($conn);
 ?>
 </body>
 </html>
